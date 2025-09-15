46176e45fe13b130a8239d42877e25f95a7dc9c5
toochinos <toochinos@example.com>
2025-09-15 17:38:38 +1000

    Update SaaS with new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2302,7 +2302,7 @@ export default function Home() {
                 <span 
                   className="absolute bottom-2 right-3 text-xs text-white/70 font-medium"
                 >
-                  Version 1.0
+                  V2.0
                 </span>
               </div>
             </div>
